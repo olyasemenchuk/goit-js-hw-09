9ff9a3d6761b9a47685eec69db948d6da2d6a63d
Olia <olyasemenchuk13@gmail.com>
2026-04-19 23:02:28 +0300

    finished hw

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -2,20 +2,39 @@ const formData = {
     email: "",
     message: ""
 }
-// const { email, message } = formData;
 
 const key = "feedback-form-state";
-
 const formEl = document.querySelector(".feedback-form");
+const emailInput = formEl.querySelector("input");
+const messageTextarea = formEl.querySelector("textarea");
+
 formEl.addEventListener("input", onFormHandler);
+formEl.addEventListener("submit", onFormSubmit);
 
 function onFormHandler(event) {
-
     const value = event.target.value.trim();
 
     formData[event.target.name] = value;
-    
-    console.log(formData);
+    localStorage.setItem(key, JSON.stringify(formData));
 }
 
+function onFormSubmit(event) {
+    event.preventDefault();
+    if (formData.email.length === 0 || formData.message.length === 0) {
+        alert('Fill please all fields')
+        return
+    }
+    console.log(formData);
+    event.target.reset();
+    localStorage.removeItem(key);
+}
+
+const savedValue = localStorage.getItem(key)
+if (savedValue) {
+    const savedObj = JSON.parse(savedValue);
+    emailInput.value = savedObj.email;
+    messageTextarea.value = savedObj.message;
+    formData.email = emailInput.value;
+    formData.message = messageTextarea.value;
+}
 
