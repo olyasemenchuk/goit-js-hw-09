100cd87da2a1021d35c78886adc7478d67392bd3
olyasemenchuk <olyasemenchuk13@gmail.com>
2026-04-19 16:55:45 +0000

    Deploying to gh-pages from @ olyasemenchuk/goit-js-hw-09@29ad8822e040c4ff9a5aac16471ee5cae78eff42 🚀

---
+++ 2-form.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */const o={email:"",message:""},r=document.querySelector(".feedback-form");r.addEventListener("input",a);function a(e){const t=e.target.value.trim();o[e.target.name]=t,console.log(o)}
+//# sourceMappingURL=2-form.js.map
